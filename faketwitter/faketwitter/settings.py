146dd3930ffd534de87e20387f6e33d6979ebd3b
Harsh Jyoriya <harshjyoriyagdg24@gmail.com>
2026-01-18 18:15:08 +0530

    Ready for deployment

--- faketwitter/faketwitter/settings.py
+++ faketwitter/faketwitter/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-9^u0k0ia9x&+%c!&_c@z#(nvksx49=s4#*qp!i&@9@=i61hqkj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tweetapp1.onrender.com']
+ALLOWED_HOSTS = [  "localhost",
+    "127.0.0.1",
+    ".onrender.com"]
 
 
 # Application definition
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,9 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-
+ 
 LOGIN_URL = '/accounts/login/'
 
 
